0690337ed2f1e36af9b4333085e04540bec07a1b
DavidIsrael <davidisrael.rj@gmail.com>
2024-07-03 20:49:21 -0300

    Mapeamento das Rotas

--- src/api/billingCycle/billingCycle.js
+++ src/api/billingCycle/billingCycle.js
@@ -1,12 +1,12 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
-const creditSchema = new mongoose.Shema({
+const creditSchema = new mongoose.Schema({
     name: {type: String, required: true},
     value: {type: Number, min: 0, required: true}
 })
 
-const debtSchema = new mongoose.Shema({
+const debtSchema = new mongoose.Schema({
     name: {type: String, required: true},
     value: {type:Number, min: 0, required: true},
     status: {type: String, required: false, uppercase: true,
